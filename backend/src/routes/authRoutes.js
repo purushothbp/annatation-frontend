d8b378c2996e911923527b80aeb7967de829f478
purushothbp <72174403+purushothbp@users.noreply.github.com>
2025-11-13 00:53:59 +0530

    Delete backend directory

--- backend/src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const { register, login, me } = require('../controllers/authController');
-
-module.exports = async function authRoutes(fastify) {
-  fastify.post('/register', register);
-  fastify.post('/login', login);
-  fastify.get('/me', { preValidation: [fastify.authenticate] }, me);
-};
